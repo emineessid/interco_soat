3eede16dc5612c833fe01b180bd8ab09d5612f95
E² <emine.essid@esprit.tn>
2018-12-10 10:43:32 +0100

    init interco app

---
+++ app/src/main/java/com/interco/e/soatintercoapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.interco.e.soatintercoapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.interco.e.soatintercoapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
