d7c0e41dd3d6de1dabddf2c02571bbc5c252be0c
Unknown <emine.essid@esprit.tn>
2018-12-10 15:07:15 +0100

    create real repesotery without dependency injection

---
+++ app/src/main/java/com/interco/e/soatintercoapp/data/repository/ForcastREpository.kt
@@ -0,0 +1,11 @@
+package com.interco.e.soatintercoapp.data.repository
+
+import androidx.lifecycle.LiveData
+import com.interco.e.soatintercoapp.data.db.unitLocalised.UnitSpecificCurrentWeatherEnty
+
+/**
+ * Created by emine on 10/12/2018.
+ */
+interface ForcastREpository {
+    suspend fun  getCurrentWeather(metric : Boolean):LiveData< out UnitSpecificCurrentWeatherEnty>
+}
---
+++ app/src/main/java/com/interco/e/soatintercoapp/data/repository/ForcastRepositoryImpl.kt
@@ -0,0 +1,58 @@
+package com.interco.e.soatintercoapp.data.repository
+
+import androidx.lifecycle.LiveData
+import com.interco.e.soatintercoapp.data.db.CurrentWeatherDao
+import com.interco.e.soatintercoapp.data.db.unitLocalised.UnitSpecificCurrentWeatherEnty
+import com.interco.e.soatintercoapp.data.network.WeatherNetworkDataSource
+import com.interco.e.soatintercoapp.data.network.response.CurrentWeatherResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.threeten.bp.ZonedDateTime
+import java.util.*
+
+class ForcastRepositoryImpl(
+    private val currentWeatherDao: CurrentWeatherDao,
+    private val weatherNetworkDataSource: WeatherNetworkDataSource
+) : ForcastREpository {
+
+    init {
+        weatherNetworkDataSource.downloadedCurrentWeather.observeForever { newCurrentWeather ->
+            persistFeachedCurrentWeather(newCurrentWeather)
+        }
+    }
+
+    override suspend fun getCurrentWeather(metric: Boolean): LiveData<out UnitSpecificCurrentWeatherEnty> {
+        return withContext(Dispatchers.IO) {
+            initWeatherData()
+            return@withContext if (metric) currentWeatherDao.getWeatherMetric()
+            else
+                currentWeatherDao.getWeatherImperial()
+        }
+    }
+
+    private fun persistFeachedCurrentWeather(feachedWeather: CurrentWeatherResponse) {
+        GlobalScope.launch(Dispatchers.IO) {
+            currentWeatherDao.upsert(feachedWeather.currentWeatherEntry)
+        }
+    }
+
+    private suspend fun initWeatherData() {
+//yes always needed for now ! and the value will be always true
+        if (isFeachNeeded(ZonedDateTime.now().minusHours(1))) {
+            feachCurrentWeather()
+        }
+    }
+
+    private suspend fun feachCurrentWeather() {
+
+        weatherNetworkDataSource.feachCurrentWeather("Paris", Locale.getDefault().language)
+
+    }
+
+    private fun isFeachNeeded(lastFechTime: ZonedDateTime): Boolean {
+        val thirtyMinutesAgo = ZonedDateTime.now().minusMinutes(30)
+        return lastFechTime.isBefore(thirtyMinutesAgo)
+    }
+}
