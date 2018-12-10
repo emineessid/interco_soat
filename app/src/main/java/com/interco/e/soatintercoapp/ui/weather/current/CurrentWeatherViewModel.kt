bafb5427e4d99e38b8c4e3c23489d2150a19082d
Unknown <emine.essid@esprit.tn>
2018-12-10 11:03:51 +0100

    add needed gragment

---
+++ app/src/main/java/com/interco/e/soatintercoapp/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.interco.e.soatintercoapp.ui.weather.current
+
+import androidx.lifecycle.ViewModel;
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
